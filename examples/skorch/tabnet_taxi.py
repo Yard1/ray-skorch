fa5a4c6bd4b53f5a8b2f50dc101876beb4deacaa
Antoni Baum <antoni.baum@protonmail.com>
2021-11-05 00:15:31 +0000

    Gradient clipping in example

--- examples/skorch/tabnet_taxi.py
+++ examples/skorch/tabnet_taxi.py
@@ -1,23 +1,18 @@
 import argparse
 
-import numpy as np
-import pandas as pd
-from skorch.callbacks.base import Callback
-
 from torch import nn
 import ray
 import os
 
 from ray.util.dask import ray_dask_get
 import dask
-import dask.array as da
 import dask.dataframe as dd
 from dask_ml.preprocessing import StandardScaler
 
 import ray.data
 import torch
-import torch.nn.functional as F
 
+from skorch.callbacks import GradientNormClipping
 from ray_sklearn.skorch_approach.base import RayTrainNeuralNet
 from ray_sklearn.models.tabnet import TabNet
 
@@ -44,8 +39,8 @@ if __name__ == "__main__":
     dask.config.set(scheduler=ray_dask_get)
     scaler = StandardScaler(copy=False)
     dask_df = scaler.fit_transform(
-        dd.read_csv(os.path.expanduser("~/data/train.csv"))[features +
-                                                            [target]].dropna())
+        dd.read_csv(os.path.expanduser("~/data/train.csv"))[
+            features + [target]].dropna().astype("float32"))
     dataset = ray.data.from_dask(dask_df)
 
     print(dataset)
@@ -53,9 +48,13 @@ if __name__ == "__main__":
 
     dtypes = [torch.float] * len(features)
 
+    class _GradientNormClipping(GradientNormClipping):
+        _on_all_ranks = True
+
     reg = RayTrainNeuralNet(
         TabNet,
         criterion=nn.MSELoss,
+        optimizer=torch.optim.Adam,
         num_workers=4,
         module__input_dim=len(features),
         module__output_dim=1,
@@ -63,6 +62,7 @@ if __name__ == "__main__":
         lr=0.02,
         batch_size=1024 / 4,
         verbose=5,
+        callbacks=[_GradientNormClipping(1.0)],
         device="cuda",
         iterator_train__feature_columns=features,
         iterator_valid__feature_columns=features,
