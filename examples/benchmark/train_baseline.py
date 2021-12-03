5bc89b9c153a4dddbcbf4b7090ec64d16f6813ee
Matthew Deng <matthew.j.deng@gmail.com>
2021-12-02 18:00:02 -0800

    additional fixes

--- examples/benchmark/train_baseline.py
+++ examples/benchmark/train_baseline.py
@@ -1,21 +1,76 @@
 import argparse
+import json
+import numbers
 import os
+import time
+from typing import List, Dict
 
 import dask
 import dask.dataframe as dd
+import numpy as np
 import ray
 import torch
 from dask_ml.preprocessing import StandardScaler
 from ray import train
 from ray.train import Trainer
+from ray.train.callbacks import JsonLoggerCallback
 from ray.util.dask import ray_dask_get
+from ray.util.ml_utils.json import SafeFallbackEncoder
 from torch import nn
-import time
-
 
 from ray_sklearn.models.tabnet import TabNet
 
 ray.data.set_progress_bars(False)
+
+def max_and_argmax(val):
+    return np.max(val), np.argmax(val)
+
+
+def min_and_argmin(val):
+    return np.min(val), np.argmin(val)
+
+
+DEFAULT_AGGREGATE_FUNC = {
+    "mean": np.mean,
+    "median": np.median,
+    "std": np.std,
+    "max": max_and_argmax,
+    "min": min_and_argmin
+}
+
+DEFAULT_KEYS_TO_IGNORE = {
+    "epoch", "_timestamp", "_training_iteration", "train_batch_size",
+    "valid_batch_size"
+}
+
+class AggregateLogCallback(JsonLoggerCallback):
+    def handle_result(self, results: List[Dict], **info):
+        results_dict = {idx: val for idx, val in enumerate(results)}
+
+        aggregate_results = {}
+        for key, value in results_dict[0].items():
+            if key in DEFAULT_KEYS_TO_IGNORE:
+                aggregate_results[key] = value
+            elif isinstance(value, numbers.Number):
+                aggregate_key = [
+                    result[key] for result in results if key in result
+                ]
+
+                aggregate = {}
+                for func_key, func in DEFAULT_AGGREGATE_FUNC.items():
+                    aggregate[func_key] = func(aggregate_key)
+                aggregate_results[key] = aggregate
+
+
+        final_results = {}
+        final_results["raw"] = results_dict
+        final_results["aggregated"] = aggregate_results
+
+        with open(self._log_path, "r+") as f:
+            loaded_results = json.load(f)
+            f.seek(0)
+            json.dump(
+                loaded_results + [final_results], f, cls=SafeFallbackEncoder)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -43,9 +98,10 @@ if __name__ == "__main__":
         help="Sets the number of training epochs. Defaults to 5.",
     )
     parser.add_argument(
-        "--smoke-test",
-        action="store_true",
-        help="If enabled, train on a smaller set of data.",
+        "--fraction",
+        type=float,
+        default=1.0,
+        help="Fraction of data to train on",
     )
     parser.add_argument(
         "--batch-size",
@@ -83,7 +139,7 @@ if __name__ == "__main__":
     use_gpu = args.use_gpu
     address = args.address
     num_epochs = args.num_epochs
-    smoke_test = args.smoke_test
+    fraction = args.fraction
     batch_size = args.batch_size
     worker_batch_size = args.worker_batch_size
     shuffle = args.shuffle
@@ -102,8 +158,7 @@ if __name__ == "__main__":
     dask_df = scaler.fit_transform(
         dd.read_csv(os.path.expanduser("~/data/train.csv"))[
             features + [target]].dropna().astype("float32"))
-    if smoke_test:
-        dask_df = dask_df.sample(frac=0.1)
+    dask_df = dask_df.sample(frac=fraction)
     dataset = ray.data.from_dask(dask_df)
 
     print(dataset)
@@ -111,10 +166,15 @@ if __name__ == "__main__":
 
     dtypes = [torch.float] * len(features)
 
-    split = 0.8
-    split_index = int(dataset.count() * split)
-    train_dataset, validation_dataset = dataset.random_shuffle().split_at_indices(
-        [split_index])
+    val_split = 0.2
+    test_split = 0.1
+    train_split = 1 - val_split - test_split
+
+    num_records = dataset.count()
+    train_val_split = int(num_records * train_split)
+    val_test_split = int(num_records * (train_split + test_split))
+    train_dataset, validation_dataset, test_dataset = dataset.random_shuffle().split_at_indices(
+        [train_val_split, val_test_split])
 
     train_dataset_pipeline = train_dataset.repeat(num_epochs)
     if shuffle:
@@ -151,6 +211,8 @@ if __name__ == "__main__":
         criterion = nn.MSELoss()
         optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer)
+
         device = train.torch.get_device()
 
         results = []
@@ -165,6 +227,8 @@ if __name__ == "__main__":
             last_time = time.time()
 
             model.train()
+            train_train_loss = 0
+            train_num_rows = 0
             for batch_idx, (X, y) in enumerate(train_torch_dataset):
                 if debug and batch_idx % 1000 == 0:
                     curr_time = time.time()
@@ -182,12 +246,18 @@ if __name__ == "__main__":
                 torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
                 optimizer.step()
 
+                count = len(X)
+                train_train_loss += count * loss.item()
+                train_num_rows += count
+
+            train_loss = train_train_loss / train_num_rows  # TODO: should this be num batches or num rows?
+            print(f"Train epoch: [{i}], mean square error:[{train_loss}]")
+
             last_time = time.time()
 
             model.eval()
-            total_loss = 0
-            num_batches = 0
-            num_rows = 0
+            val_total_loss = 0
+            val_num_rows = 0
             with torch.no_grad():
                 for batch_idx, (X, y) in enumerate(validation_torch_dataset):
                     if debug and batch_idx % 1000 == 0:
@@ -199,14 +269,16 @@ if __name__ == "__main__":
                     y = y.to(device)
                     count = len(X)
                     pred = model(X)
-                    # total_loss += criterion(pred, y).item()
-                    # num_batches += 1
-                    total_loss += count * criterion(pred, y).item()
-                    num_rows += count
-            loss = total_loss / num_rows  # TODO: should this be num batches or num rows?
-            result = {"mean square error": loss}
-            print(f"Validation epoch: [{i}], mean square error:[{loss}]")
-            results.append(result)
+                    val_total_loss += count * criterion(pred, y).item()
+                    val_num_rows += count
+            val_loss = val_total_loss / val_num_rows  # TODO: should this be num batches or num rows?
+            print(f"Validation epoch: [{i}], mean square error:[{val_loss}]")
+
+            scheduler.step(train_loss)
+
+            train.report(train_mse=train_loss, val_mse=val_loss)
+            train.save_checkpoint(model_state_dict=model.module.state_dict())
+            results.append(val_loss)
 
         return results
 
@@ -214,7 +286,7 @@ if __name__ == "__main__":
 
     trainer = Trainer("torch", num_workers=num_workers, use_gpu=use_gpu)
     trainer.start()
-    results = trainer.run(train_func, dataset=datasets)
+    results = trainer.run(train_func, dataset=datasets, callbacks=[AggregateLogCallback()])
     trainer.shutdown()
 
 
