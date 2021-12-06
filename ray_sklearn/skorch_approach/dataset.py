1ddc8dcde50b703f9f83189223ec624f446099a1
Antoni Baum <antoni.baum@protonmail.com>
2021-12-06 20:03:04 +0000

    Add TODO

--- ray_sklearn/skorch_approach/dataset.py
+++ ray_sklearn/skorch_approach/dataset.py
@@ -53,6 +53,7 @@ class RayDataset(SkorchDataset):
             self._init_dataset(X, y)
         else:
             super().__init__(X, y=y, length=length)
+            # TODO ensure LABEL_COLUMN is not in X
             if y is not None:
                 if not is_pandas_ndframe(self.y):
                     self.y = pd.DataFrame(self.y, columns=[LABEL_COLUMN])
