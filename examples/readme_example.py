f94c1d10d7b7aa1f441f0b395d198d67602668a6
Antoni Baum <antoni.baum@protonmail.com>
2022-01-05 11:46:24 +0000

    Fix prediction serialization issues

---
+++ examples/readme_example.py
@@ -0,0 +1,46 @@
+import numpy as np
+from sklearn.datasets import make_classification
+from torch import nn
+
+from ray_skorch import RayTrainNeuralNet
+
+X, y = make_classification(1000, 20, n_informative=10, random_state=0)
+X = X.astype(np.float32)
+y = y.astype(np.int64)
+
+
+class MyModule(nn.Module):
+    def __init__(self, num_units=10, nonlin=nn.ReLU()):
+        super(MyModule, self).__init__()
+
+        self.dense0 = nn.Linear(20, num_units)
+        self.nonlin = nonlin
+        self.dropout = nn.Dropout(0.5)
+        self.dense1 = nn.Linear(num_units, num_units)
+        self.output = nn.Linear(num_units, 2)
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, X, **kwargs):
+        X = self.nonlin(self.dense0(X))
+        X = self.dropout(X)
+        X = self.nonlin(self.dense1(X))
+        X = self.softmax(self.output(X))
+        return X
+
+
+net = RayTrainNeuralNet(
+    MyModule,
+    num_workers=2,  # the only new mandatory argument
+    criterion=nn.CrossEntropyLoss,
+    max_epochs=10,
+    lr=0.1,
+    # required for classification loss funcs
+    iterator_train__unsqueeze_label_tensor=False,
+    iterator_valid__unsqueeze_label_tensor=False,
+)
+
+net.fit(X, y)
+
+# predict_proba returns a ray.data.Dataset
+y_proba = net.predict_proba(X).to_pandas()
+print(y_proba)
